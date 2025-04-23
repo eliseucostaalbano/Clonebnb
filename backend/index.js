8c5ae45e6b54808198a8f59e1bd5dcc83b045dc5
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-04-23 15:05:02 -0300

    configurando o banco de dados e modelo de usuário

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import "dotenv/config";
+import { connectDb } from './config/db.js';
+import User from './models/User.js';
 
 const app = express();
 const { PORT } = process.env;
 
-app.get('/', (req, res) => {
-  res.json('Olá Mundo!');
+app.get("/users", async (req, res) => {
+  connectDb();
+const userDoc = await User.find();
+
+    res.json(userDoc);
+
 });
+
 
 app.listen(PORT, () => {
   console.log(`Servidor está rodando na porta ${PORT}`);
-});
+})
