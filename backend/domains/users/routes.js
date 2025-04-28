989ce8d7d05e2fb465e2d6b8cca0de493ad3cc89
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-04-28 13:29:28 -0300

    adicionar rota de login para autenticação de usuários

--- backend/domains/users/routes.js
+++ backend/domains/users/routes.js
@@ -36,4 +36,25 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  connectDb();
+
+  const { email, senha } = req.body;
+  try {
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+      const senhaCorreta = bcrypt.compareSync(senha, userDoc.senha);
+       const { nome, _id } = userDoc;
+
+      senhaCorreta ? res.json({nome, email, _id}) : res.status(400).json("Senha incorreta");
+    } else {
+      res.status(400).json("Usuário não encontrado");
+    }
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+})
+
 export default router;
